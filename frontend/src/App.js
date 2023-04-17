e6f001964a9488d74e8568f8800621a9a4fcbba9
mbugua <mbuguafaimba@gmail.com>
2023-04-17 12:47:22 +0300

    added homepage route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route} from "react-router-dom"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 import LogOut from "./components/LogOut"
+import Homepage from "./components/Homepage"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       
         <Routes>
+          <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Login />}/>
           <Route path="/logout" element={<LogOut />}/>
           <Route path="/signup" element={<Signup />}/>
