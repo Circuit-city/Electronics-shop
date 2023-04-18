ec342a515e1a0322e7d3b0bc572c961ae84e5a99
Ian <chemwenokibet11@gmail.com>
2023-04-18 15:47:48 +0300

    added role column

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_082604) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_124114) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -22,11 +25,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_082604) do
     t.string "name"
     t.text "description"
     t.decimal "price"
-    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
-    t.index ["category_id"], name: "index_products_on_category_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,5 +40,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_082604) do
     t.string "roles"
   end
 
-  add_foreign_key "products", "categories"
 end
