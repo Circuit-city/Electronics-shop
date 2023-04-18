e8fb819f7a596cbdca54a3f9451c00b7e7253a24
Issa Nyale <me@email.com>
2023-04-18 09:56:31 +0300

    Added a checkout route in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 import LogOut from "./components/LogOut"
-// import Chekout from "./components/Checkout"
+import Chekout from "./components/Checkout"
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/logout" element={<LogOut />}/>
-          {/* <Route path="/checkout" element={<Chekout />}/> */}
+          <Route path="/checkout" element={<Chekout />}/>
         </Routes>
       </BrowserRouter>
     </div>
