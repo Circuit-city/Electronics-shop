62cc6219d102564f544747ffc171745f64bd383f
rose534 <wambuirose534@gmail.com>
2023-04-19 19:10:43 +0300

    homepage, navbar and css styling and fetching

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import { useNavigate } from "react-router-dom";
 import '../components/Signup.css'
+
 function Signup(){
     const [name, setname] = useState('');
     const [email,setemail] = useState('');
